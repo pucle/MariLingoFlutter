9209634f1305e45c0c1f2c1d6d3611863923fb01
pucle <ledinhphuc1408@gmail.com>
2025-07-31 16:18:31 +0700

    Update cocokata.dart

--- lib/cocokata.dart
+++ lib/cocokata.dart
@@ -1,16 +1,14 @@
 // ignore_for_file: prefer_const_constructors, unnecessary_new, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
-
 import 'MainMenu.dart';
 
-var finalScore = 0;
-var questionNumber = 0;
-var quiz = CocokKata();
-String userAnswer = " ";
+int finalScore = 0;
+int questionNumber = 0;
+final quiz = CocokKata();
 
 class CocokKata {
-  var question = [
+  final List<String> question = [
     "The dog barks",
     "I ate a sandwich",
     "The baby cried",
@@ -18,7 +16,7 @@ class CocokKata {
     "I hear the rain"
   ];
 
-  var corrAnswer = [
+  final List<String> corrAnswer = [
     "The dog barks ",
     "I ate a sandwich ",
     "The baby cried ",
@@ -31,129 +29,109 @@ class MalingCocok extends StatefulWidget {
   const MalingCocok({Key? key}) : super(key: key);
 
   @override
-  State<StatefulWidget> createState() {
-    return _MalingCocokState();
-  }
+  State<MalingCocok> createState() => _MalingCocokState();
 }
 
 class _MalingCocokState extends State<MalingCocok> {
-  String nAnswer = "";
+  final TextEditingController txt = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    var txt = TextEditingController();
     String question = quiz.question[questionNumber];
-    List l = question.split(' ');
-    l.shuffle();
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Cocok Kata"),
-          backgroundColor: Colors.red,
-        ),
-        body: Center(
+    List<String> words = List.from(question.split(' '))..shuffle();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Cocok Kata"),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
                 "Susunlah kata-kata di bawah sesuai dengan tata bahasa yang benar!",
                 textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                ),
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
               ),
-              Padding(padding: EdgeInsets.only(top: 40.0)),
+              SizedBox(height: 40),
               Container(
                 width: 350,
                 child: TextFormField(
                   controller: txt,
                   enabled: false,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(),
-                      ),
-                      hintText: 'Your answer will be displayed here'),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Your answer will be displayed here',
+                  ),
                 ),
               ),
-              Padding(padding: EdgeInsets.only(top: 30.0)),
-              for (int i = 0; i < l.length; i++)
-                Container(
-                    child: Column(
-                  children: [
-                    MaterialButton(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12.0)),
-                      minWidth: 200,
-                      height: 50,
-                      color: Colors.red,
-                      onPressed: () {
-                        {
-                          txt.text += l[i];
-                          txt.text += " ";
-                        }
-                      },
-                      child: Text(
-                        l[i],
-                        style: TextStyle(
-                          fontSize: 24,
-                          color: Colors.black,
+              SizedBox(height: 30),
+              ...words.map((word) => Column(
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12)),
+                          minimumSize: Size(200, 50),
+                          backgroundColor: Colors.red,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            txt.text += '$word ';
+                          });
+                        },
+                        child: Text(
+                          word,
+                          style: TextStyle(fontSize: 24, color: Colors.black),
                         ),
                       ),
-                    ),
-                    Padding(padding: EdgeInsets.only(top: 10.0)),
-                  ],
-                )),
-              Padding(padding: EdgeInsets.only(top: 10.0)),
+                      SizedBox(height: 10),
+                    ],
+                  )),
+              SizedBox(height: 10),
               ElevatedButton(
-                  onPressed: () {
-                    txt.text = '';
-                  },
-                  child: Text('Reset')),
+                onPressed: () => setState(() => txt.clear()),
+                child: Text('Reset'),
+              ),
+              SizedBox(height: 10),
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
-                    shape: new RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(12.0),
-                    ),
-                    fixedSize: const Size(200, 50),
-                    primary: Colors.green),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  fixedSize: Size(200, 50),
+                  backgroundColor: Colors.green,
+                ),
                 onPressed: () {
-                  setState(() {
-                    nAnswer = txt.text;
-                  });
-                  if (txt.text == quiz.corrAnswer[questionNumber]) {
-                    print("Correct");
-                    finalScore++;
-                    debugPrint(quiz.corrAnswer[questionNumber]);
-                    debugPrint(nAnswer);
-                  } else {
-                    print("Wrong");
-                    debugPrint(quiz.corrAnswer[questionNumber]);
-                    debugPrint(nAnswer);
-                  }
+                  String userAnswer = txt.text;
+                  bool isCorrect = userAnswer == quiz.corrAnswer[questionNumber];
+
+                  if (isCorrect) finalScore++;
+
+                  debugPrint(isCorrect ? "Correct" : "Wrong");
+                  debugPrint("Expected: ${quiz.corrAnswer[questionNumber]}");
+                  debugPrint("User: $userAnswer");
+
                   txt.clear();
                   updateQuestion();
                 },
                 child: Text(
                   'Submit',
-                  style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.black,
-                  ),
+                  style: TextStyle(fontSize: 24, color: Colors.black),
                 ),
               ),
-              Padding(padding: EdgeInsets.only(top: 40.0)),
+              SizedBox(height: 40),
               Text(
                 "Score : $finalScore",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
             ],
           ),
         ),
       ),
-      theme: new ThemeData(scaffoldBackgroundColor: Colors.grey[700]),
+      backgroundColor: Colors.grey[700],
     );
   }
 
@@ -161,11 +139,9 @@ class _MalingCocokState extends State<MalingCocok> {
     setState(() {
       if (questionNumber == quiz.question.length - 1) {
         Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => Summary(
-                      score: finalScore,
-                    )));
+          context,
+          MaterialPageRoute(builder: (context) => Summary(score: finalScore)),
+        );
       } else {
         questionNumber++;
       }
@@ -176,90 +152,62 @@ class _MalingCocokState extends State<MalingCocok> {
 class Summary extends StatelessWidget {
   final int score;
 
-  var child;
-  Summary({Key? key, required this.score}) : super(key: key);
+  const Summary({Key? key, required this.score}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Cocok Kata"),
-          backgroundColor: Colors.red,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              InkWell(
-                child: Container(
-                    child: (score >= 3)
-                        ? Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              new Image.asset(
-                                'assets/good.png',
-                                height: 145,
-                              ),
-                            ],
-                          )
-                        : Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              new Image.asset(
-                                'assets/sad.png',
-                                height: 250,
-                              ),
-                            ],
-                          )),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Cocok Kata"),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              score >= 3 ? 'assets/good.png' : 'assets/sad.png',
+              height: score >= 3 ? 145 : 250,
+            ),
+            SizedBox(height: 20),
+            Text(
+              score >= 3 ? "Good Job" : "Nice try",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 50,
+                color: score >= 3 ? Colors.green : Colors.red,
               ),
-              if (score >= 3)
-                Text(
-                  "Good Job",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 50.0,
-                      color: Colors.green),
-                )
-              else
-                Text(
-                  "Nice try",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 50.0,
-                      color: Colors.red),
-                ),
-              Padding(padding: EdgeInsets.all(30.0)),
-              Text(
-                "Final Score: $score",
-                style: const TextStyle(
-                    fontWeight: FontWeight.bold, fontSize: 30.0),
-              ),
-              Padding(padding: EdgeInsets.all(30.0)),
-              MaterialButton(
+            ),
+            SizedBox(height: 30),
+            Text(
+              "Final Score: $score",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            ),
+            SizedBox(height: 30),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
                 shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12.0)),
-                minWidth: 300,
-                height: 50,
-                color: Colors.red,
-                onPressed: () {
-                  questionNumber = 0;
-                  finalScore = 0;
-                  print("$finalScore");
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const MainMenu()),
-                  );
-                },
-                child: const Text(
-                  "Back to Main Menu",
-                  style: TextStyle(fontSize: 20.0, color: Colors.black),
-                ),
-              )
-            ],
-          ),
+                    borderRadius: BorderRadius.circular(12)),
+                minimumSize: Size(300, 50),
+                backgroundColor: Colors.red,
+              ),
+              onPressed: () {
+                questionNumber = 0;
+                finalScore = 0;
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MainMenu()),
+                );
+              },
+              child: Text(
+                "Back to Main Menu",
+                style: TextStyle(fontSize: 20, color: Colors.black),
+              ),
+            ),
+          ],
         ),
       ),
-      theme: new ThemeData(scaffoldBackgroundColor: Colors.grey[700]),
+      backgroundColor: Colors.grey[700],
     );
   }
 }
